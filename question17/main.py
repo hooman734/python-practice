ea1f1bdb432f3110f3c6d10dccdb672b6034b78a
Hooman <hesamyan@outlook.com>
2022-06-07 00:02:10 +0400

    revised the code of question 17

--- question17/main.py
+++ question17/main.py
@@ -9,63 +9,22 @@ Convert to base n, and output the result.
 
 
 import numbers 
-import string 
-import re 
 
 
-def convert_to_decimal(input_number, base):
-    alphanumerical_list = {'A': 10,
-                           'B': 11,
-                           'C': 12,
-                           'D': 13,
-                           'E': 14,
-                           'F': 15,
-                           'G': 16,
-                           'H': 17,
-                           'I': 18,
-                           'J': 19,
-                           'K': 20,
-                           'L': 21,
-                           'M': 22,
-                           'N': 23,
-                           'O': 24,
-                           'P': 25,
-                           'Q': 26,
-                           'R': 27,
-                           'S': 28,
-                           'T': 29,
-                           'U': 30,
-                           'V': 31,
-                           'W': 32,
-                           'X': 33,
-                           'Y': 34,
-                           'Z': 35}
+def convert_decimal_to_base_n(decimal_number, base_n):
+    numerator: numbers = int(decimal_number)
+    denominator: numerator = int(base_n)
+    output: list = []
 
-    number = re.findall(r'\d|[A-Z]', str(input_number).strip().upper())
+    while numerator >= denominator:
+        calculate = numerator % denominator
+        output.insert(0, str(calculate))
+        numerator = int(numerator / denominator)
 
-    base = str(base).strip()
-    if int(base) < 2 or not base.isdecimal():
-        raise Exception("The Base is Invalid!")
-    else:
-        base = int(base)
+    output.insert(0, str(numerator))
 
-    output: numbers = 0
-
-    for i in range(len(number)):
-        digit: string = number[-1 - i]
-
-        if digit.isupper():
-            digit = int(alphanumerical_list[digit])
-        else:
-            digit = int(digit)
-
-        if digit >= base:
-            raise Exception("The Input Number is Invalid!")
-
-        output += int(digit) * base ** i
-
-    print(f"The Input Number ({input_number} in base {base}) in Decimal is {output}")
+    print(" ".join(output))
 
 
 if __name__ == '__main__':
-    convert_to_decimal("A157BC019K", 50)
+    convert_decimal_to_base_n(1024, 2)
